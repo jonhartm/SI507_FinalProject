92db1d202bf2755a1d69cab8bb225467aca6f9d8
jonhartm <jonhartm@umich.edu>
2018-04-16 00:59:02 -0400

    added scraping the Academy Awards Page of Wikipedia and loading into db

---
+++ load_scraping.py
@@ -0,0 +1,48 @@
+from caching import *
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+import sqlite3
+
+Database_Name = 'movies.db'
+
+def AAForBestPicture():
+    url = 'https://en.wikipedia.org/wiki/Academy_Award_for_Best_Picture'
+    AA_Cache = CacheFile('WikipediaCache.json', print_info=True)
+    BestPictures = AA_Cache.CheckCache_Soup(url, strainer=SoupStrainer(class_="wikitable"))
+
+    winners = []
+    nominees = []
+
+    for decade in BestPictures.find_all(class_="wikitable"):
+        # input(decade)
+        for table_row in decade.find_all("tr", style={"background:#FAEB86"}):
+            row = table_row.find("i")
+            if row:
+                winners.append(row.text)
+        # input(decade)
+        for table_row in decade.find_all("tr"):
+            row = table_row.find("i")
+            if row and row.text not in winners:
+                nominees.append(row.text)
+
+    # print(winners)
+    # print(nominees)
+    UpdateDatabase(winners, nominees)
+
+def UpdateDatabase(winners, nominees):
+    print("Adding Academy Award Winners/Nominees...")
+    conn = sqlite3.connect(Database_Name)
+    cur = conn.cursor()
+
+    for title in winners:
+        statement = 'UPDATE Films SET AcademyAward = 1 WHERE Title == "' + title + '"';
+        cur.execute(statement)
+
+    for title in nominees:
+        statement = 'UPDATE Films SET AcademyAward = 0 WHERE Title == "' + title + '"';
+        cur.execute(statement)
+
+    conn.commit()
+    conn.close()
+
+AAForBestPicture()
