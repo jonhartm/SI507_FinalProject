6c2f4db2f4caaaa948f4ee2d3bee65f084dd6578
jonhartm <jonhartm@umich.edu>
2018-04-18 15:06:42 -0400

    more complex graphs

--- create_plotly.py
+++ create_plotly.py
@@ -1,13 +1,31 @@
 import plotly.plotly as py
 import plotly.offline as offline
 import plotly.graph_objs as go
+from query_builder import selectQueryBuilder
 
 def GetDiv():
-    data = [go.Bar(
-                x=['giraffes', 'orangutans', 'monkeys'],
-                y=[20, 14, 23]
-        )]
+    trace1 = go.Bar(
+        x=['giraffes', 'orangutans', 'monkeys'],
+        y=[20, 14, 23],
+        name='SF Zoo'
+    )
+    trace2 = go.Bar(
+        x=['giraffes', 'orangutans', 'monkeys'],
+        y=[12, 18, 29],
+        name='LA Zoo'
+    )
 
-    fig = go.Figure(data=data)
+    data = [trace1, trace2]
+    layout = go.Layout(
+        barmode='stack'
+    )
+
+    fig = go.Figure(data=data, layout=layout)
 
     return offline.plot(fig, show_link=False, output_type="div", include_plotlyjs=False)
+
+def Graph_AAWinners(count=10, show_nominations=True):
+    pass
+
+def MakeBarTrace():
+    pass
