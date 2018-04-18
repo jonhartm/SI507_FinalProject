d6cfcdbafc19ad56601d7bf9a00441a58da77422
jonhartm <jonhartm@umich.edu>
2018-04-18 16:59:24 -0400

    Calling the Academy Award Basic Stats completed

--- create_plotly.py
+++ create_plotly.py
@@ -1,38 +1,77 @@
-import plotly.plotly as py
+import sqlite3 as sqlite
 import plotly.offline as offline
 import plotly.graph_objs as go
 from query_builder import selectQueryBuilder
+from settings import *
 
-def GetDiv():
-    trace1 = go.Bar(
-        x=['giraffes', 'orangutans', 'monkeys'],
-        y=[20, 14, 23],
-        name='SF Zoo'
-    )
-    trace2 = go.Bar(
-        x=['giraffes', 'orangutans', 'monkeys'],
-        y=[12, 18, 29],
-        name='LA Zoo'
+class BarTrace():
+    def __init__(self, name):
+        self.name = name
+        self.labels = []
+        self.values = []
+
+    def Get(self):
+        return go.Bar(
+            x = self.labels,
+            y = self.values,
+            name = self.name
+        )
+
+def Graph_AAWinners(sort="wins", count=10, show_nominations=True):
+    conn = sqlite.connect(DATABASE_NAME)
+    cur = conn.cursor()
+
+    if sort == "noms":
+        order = ['AA_Nominations DESC', 'AA_Wins DESC']
+    else:
+        order = ['AA_Wins DESC', 'NomNotWon DESC']
+
+    query = selectQueryBuilder(
+        columns = ['Title','Release','AA_Wins', '(AA_Nominations - AA_Wins) AS NomNotWon'],
+        table = 'Film',
+
+        order_by = order,
+        limit=count
     )
 
-    data = [trace1, trace2]
-    layout = go.Layout(
-        barmode='stack'
-    )
+    cur.execute(query)
+    trace1 = BarTrace("Wins")
+    trace2 = BarTrace("Nominations")
+
+    for row in cur:
+        trace1.labels.append(row[0])
+        trace1.values.append(row[2])
+
+        trace2.labels.append(row[0])
+        trace2.values.append(row[3])
+
+    data = [trace1.Get()]
+    if show_nominations:
+        data.append(trace2.Get())
+
+    layout = go.Layout(barmode='stack')
 
     fig = go.Figure(data=data, layout=layout)
 
     return offline.plot(fig, show_link=False, output_type="div", include_plotlyjs=False)
 
-def Graph_AAWinners(count=10, show_nominations=True):
-    pass
-
-def MakeBarTrace():
-    pass
-
-print(selectQueryBuilder(
-    columns = ['Title','Release','AA_Wins'],
-    table = 'Film',
-    order_by ='AA_Wins',
-    limit=['top',10]
-))
+# print("Top films by User Rating")
+# sub_query = selectQueryBuilder(
+#     columns = ['MovieID', 'AVG(Rating) AS UserRatings'],
+#     table = 'Ratings',
+#     group_by = 'MovieID',
+#     filter = ['COUNT(*)', '>', 30]
+# )
+# print(selectQueryBuilder(
+#     columns = [
+#         'Title',
+#         'Release',
+#         '((Rating_IMDB*10)+(Rating_RT*1)+Rating_MC)/3 AS AvgCriticRating',
+#         '(UserRatings*20) AS AvgUserRating'
+#     ],
+#     table = 'Film',
+#     joins = "JOIN ("+sub_query+") ON FilmID == MovieID",
+#     filter = ["AvgCriticRating", "IS NOT", "NULL"],
+#     order_by = "UserRatings",
+#     limit = ['top', 50]
+# ))
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from init_Database import ResetDatabase
 from create_plotly import *
 
@@ -7,9 +7,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return '<h1>Welcome!</h1>'
+
+@app.route('/AA_Winners', methods=['GET', 'POST'])
+def AA_Winners():
+    count=10
+    show_nom = True
+    sort="wins"
+    if request.method == "POST":
+        count = request.form['count']
+        if count == '':
+            count = 10
+        show_nom = request.form.get('show_nom') # https://stackoverflow.com/questions/31859903/get-the-value-of-a-checkbox-in-flask
+        sort=request.form['sort_by']
+        display_graph = Graph_AAWinners(sort=sort, count=count, show_nominations=show_nom)
+    else:
+        #display the graph with defaults
+        display_graph = Graph_AAWinners()
     return render_template(
-        "index.html",
-        graph=GetDiv()
+        "AA_Winners.html",
+        graph=display_graph,
+        count=count,
+        show_nom=show_nom,
+        sort=sort
         )
 
 if __name__=="__main__":
